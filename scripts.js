e6f8c78e9bdde8544753936369624ab8446215f1
timerespa <timerespa@interia.pl>
2025-03-02 14:23:15 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -130,12 +130,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
     init();
 });
-function updateClock() {
-    let now = new Date();
-    let hours = now.getHours().toString().padStart(2, '0');
-    let minutes = now.getMinutes().toString().padStart(2, '0');
-    let seconds = now.getSeconds().toString().padStart(2, '0');
-    document.getElementById("clock").innerHTML = `${hours}:${minutes}:${seconds}`;
-}
-setInterval(updateClock, 1000);
-updateClock();
